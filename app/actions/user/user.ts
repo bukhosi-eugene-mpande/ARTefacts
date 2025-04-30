5922e4ac4842ab9af49288537656453113a4e2bf
eugenempande <eugene.mpande@gofuseit.com>
2025-04-30 19:22:42 +0200

    added ability to change avatar

--- app/actions/user/user.ts
+++ app/actions/user/user.ts
@@ -1,20 +1,10 @@
 'use server';
+import { decodeJWT } from '@/app/actions/utilities/utils';
+
 import { User } from './user.types';
 
 const API_URL = process.env.API_URL!;
 const API_KEY = process.env.API_KEY!;
-
-function decodeJWT(token: string): { sub: string } {
-  try {
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const payload = Buffer.from(base64, 'base64').toString('utf-8');
-
-    return JSON.parse(payload);
-  } catch {
-    throw new Error('Invalid JWT token');
-  }
-}
 
 export async function getUserDetails(jwt: string): Promise<User> {
   try {
@@ -40,6 +30,43 @@ export async function getUserDetails(jwt: string): Promise<User> {
     const data: User = await response.json();
 
     return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function updateAvatar(jwt: string, key: string): Promise<User> {
+  try {
+    const decoded = decodeJWT(jwt);
+    const user_id = decoded.sub;
+
+    const endpoint = `${API_URL}/artefacts`;
+
+    const requestBody = {
+      user_id: user_id,
+      key: key,
+    };
+
+    const response = await fetch(endpoint, {
+      method: 'Post',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': API_KEY,
+        Authorization: `Bearer ${jwt}`,
+      },
+      cache: 'no-store',
+      body: JSON.stringify(requestBody),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json().catch(() => ({}));
+
+      throw new Error(
+        `HTTP error! status: ${response.status}, message: ${errorData.message || 'Unknown error'}`
+      );
+    }
+
+    return getUserDetails(jwt);
   } catch (error) {
     throw error;
   }
