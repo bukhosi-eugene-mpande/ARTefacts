d694dd1803b311e45a8963ef6028f085c646e5c8
Linda Masia <masialr3x@gmail.com>
2025-05-21 03:36:30 +0200

    Update page.tsx

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,56 +1,53 @@
 'use client';
+
 import Image from 'next/image';
-import { motion } from 'framer-motion';
-import { useState } from 'react';
+import { useState, FormEvent } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
-import { setTokens } from '@/lib/authStorage';
 import logo from '@/public/assets/logo.svg';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
-import { handleSignIn } from '@/lib/cognitoActions'; // Import the handleSignIn function
-
+import { setTokens } from '@/lib/authStorage';
+import { handleSignIn } from '@/lib/cognitoActions';
 import ConfigureAmplifyClientSide from '../../../lib/amplify-cognito-config';
 
 const Login = () => {
   const router = useRouter();
   const [usernameOrEmail, setUsernameOrEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState<string | null>(null); // State to store error message
-  const [loading, setLoading] = useState(false); // State to track loading status
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
 
-  const handleSignInClick = async (e: React.FormEvent) => {
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     setError(null);
 
-    const trimmedEmail = usernameOrEmail.trim();
-    const trimmedPassword = password.trim();
+    const email = usernameOrEmail.trim();
+    const pass = password.trim();
 
-    if (!trimmedEmail || !trimmedPassword) {
-      if (!trimmedEmail && !trimmedPassword) {
-        setError('Please fill in email/username and password.');
-      } else if (!trimmedEmail) {
-        setError('Please fill in email/username.');
-      } else {
-        setError('Please fill in password.');
-      }
-
+    if (!email || !pass) {
+      setError(
+        !email && !pass
+          ? 'Please fill in email/username and password.'
+          : !email
+            ? 'Please fill in email/username.'
+            : 'Please fill in password.'
+      );
       return;
     }
 
     setLoading(true);
 
     const formData = new FormData();
-
-    formData.append('email', trimmedEmail);
-    formData.append('password', trimmedPassword);
+    formData.append('email', email);
+    formData.append('password', pass);
 
     const result = await handleSignIn(formData);
 
     if (typeof result === 'string') {
       setError(result);
-    } else if (result && result.AccessToken && result.RefreshToken) {
+    } else if (result?.AccessToken && result?.RefreshToken) {
       setTokens(result);
       router.push('/pages/home');
     }
@@ -61,43 +58,38 @@ const Login = () => {
   return (
     <>
       <ConfigureAmplifyClientSide />
-
-      <div className="flex flex-col items-center justify-center bg-[#231209]">
-        <Image alt="Logo" className="mb-8" src={logo} />
+      <div className="flex flex-col items-center justify-center">
+        <Image alt="Logo" src={logo} className="mb-8" />
 
         <form
-          className="shadow-input w-full max-w-sm space-y-4 overflow-hidden rounded-[5%] bg-white px-6 py-6 dark:bg-[#141313]"
-          onSubmit={handleSignInClick}
+          onSubmit={handleSubmit}
+          className="shadow-input flex h-auto w-full flex-col items-center justify-center space-y-10 overflow-hidden rounded-[5%] bg-white px-6 pb-6 pt-12 dark:bg-[#141313] sm:w-[400px] md:h-[450px] md:w-[550px]"
         >
           <LabelInputContainer>
-            <Label className="text-left" htmlFor="firstname">
-              Username or Email
-            </Label>
+            <Label htmlFor="username">Username or Email</Label>
             <Input
-              className="font-garamond font-bold"
-              id="firstname"
-              placeholder="Johnny"
+              id="username"
               type="text"
+              placeholder="Johnny"
               value={usernameOrEmail}
               onChange={(e) => setUsernameOrEmail(e.target.value)}
+              className="rounded-2xl bg-white font-garamond font-bold"
             />
           </LabelInputContainer>
 
-          <LabelInputContainer className="mb-4">
-            <Label className="text-left" htmlFor="firstname">
-              Password
-            </Label>
+          <LabelInputContainer>
+            <Label htmlFor="password">Password</Label>
             <Input
-              className="w-full rounded-md border border-gray-300 px-4 py-3 font-garamond font-bold text-black focus:outline-none focus:ring-2 focus:ring-[#d4af7a]"
-              placeholder="Password"
+              id="password"
               type="password"
+              placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              className="w-full rounded-2xl border border-gray-300 px-4 py-3 font-garamond font-bold text-black focus:outline-none focus:ring-2 focus:ring-[#d4af7a]"
             />
           </LabelInputContainer>
 
           {loading && <p className="text-yellow-500">Logging in...</p>}
-
           {error && (
             <p className="text-red-500">
               {error.includes('INCORRECT USERNAME OR PASSWORD')
@@ -107,48 +99,38 @@ const Login = () => {
           )}
 
           <button
-            className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-[#bd9b73] to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-[#614f3b] dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
             type="submit"
+            className="group/btn relative block h-10 w-full rounded-2xl bg-gradient-to-br from-[#bd9b73] to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:from-[#614f3b] dark:to-zinc-900"
           >
             Login
             <BottomGradient />
           </button>
-        </form>
 
-        <p className="mt-4 max-w-sm text-sm text-neutral-600 dark:text-neutral-300">
-          Don&apos;t have an account?{' '}
-          <Link className="text-[#bd9b73]" href="/auth/signup">
-            Sign Up.
-          </Link>
-        </p>
+          <p className="mt-4 text-center text-sm text-neutral-600 dark:text-neutral-300">
+            Don&apos;t have an account?{' '}
+            <Link href="/auth/signup" className="text-[#bd9b73]">
+              Sign Up.
+            </Link>
+          </p>
+        </form>
       </div>
     </>
   );
 };
 
-const BottomGradient = () => {
-  return (
-    <>
-      <span className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100" />
-      <span className="absolute inset-x-10 -bottom-px mx-auto block h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover/btn:opacity-100" />
-    </>
-  );
-};
+const BottomGradient = () => (
+  <>
+    <span className="absolute inset-x-0 -bottom-px h-px w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100" />
+    <span className="absolute inset-x-10 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover/btn:opacity-100" />
+  </>
+);
 
 const LabelInputContainer = ({
   children,
-  className,
+  className = 'flex w-full flex-col space-y-2',
 }: {
   children: React.ReactNode;
   className?: string;
-}) => {
-  return (
-    <div
-      className={((className = 'flex w-full flex-col space-y-2'), className)}
-    >
-      {children}
-    </div>
-  );
-};
+}) => <div className={className}>{children}</div>;
 
 export default Login;
