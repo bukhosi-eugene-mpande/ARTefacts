3f4468df9b63eef8d660e237df2b2786a7087d78
Linda Masia <masialr3x@gmail.com>
2025-06-19 14:28:56 +0200

    fixed signup responsiveness

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -111,158 +111,155 @@ export default function SignupModal() {
       )}
       <ConfigureAmplifyClientSide />
       <div className="flex min-h-screen w-full bg-cover bg-center">
-        <div className="w-1/2" />
         <div className="flex w-full items-center justify-end">
-          <div className="flex min-h-screen flex-col bg-cover bg-center">
-            <form
-              className="bg-opacity-97 flex min-h-screen w-full flex-col items-center justify-center space-y-5 bg-[#231209] px-6 shadow-lg"
-              onSubmit={handleSubmit}
-            >
-              <Image
-                alt="Logo"
-                className="mx-10 h-[150px] w-auto"
-                height={150}
-                src={logo}
-                width={300}
+          <form
+            className="bg-opacity-97 flex min-h-screen w-full flex-col items-center justify-center space-y-5 bg-[#231209] px-6 shadow-lg md:w-[33.3333vw]"
+            onSubmit={handleSubmit}
+          >
+            <Image
+              alt="Logo"
+              className="mx-10 h-[150px] w-auto"
+              height={150}
+              src={logo}
+              width={300}
+            />
+            <div className="flex w-[80%] flex-col space-y-2">
+              <div className="flex flex-col space-y-2">
+                <Label
+                  className="w-full text-left text-lg text-white"
+                  htmlFor="firstname"
+                >
+                  First name
+                </Label>
+                <Input
+                  className="bg-[#e5d1b4] px-4 py-3 font-garamond text-lg font-semibold text-black placeholder:text-gray-700"
+                  id="firstname"
+                  placeholder="Johnny"
+                  type="text"
+                  value={firstname}
+                  onChange={(e) => setFirstname(e.target.value)}
+                />
+              </div>
+
+              <div className="flex flex-col space-y-1">
+                <Label
+                  className="w-full text-left text-lg text-white"
+                  htmlFor="username"
+                >
+                  Username
+                </Label>
+                {usernameError && (
+                  <p className="mb-1 text-xs text-red-500">{usernameError}</p>
+                )}
+                <Input
+                  className="bg-[#e5d1b4] px-4 py-3 font-garamond text-lg font-semibold text-black placeholder:text-gray-700"
+                  id="username"
+                  placeholder="Johnny_"
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="flex w-[80%] flex-col space-y-2">
+              <Label
+                className="w-full text-left text-lg text-white"
+                htmlFor="email"
+              >
+                Email
+              </Label>
+              <Input
+                className="w-full bg-[#e5d1b4] px-4 py-3 font-garamond text-lg font-semibold text-black placeholder:text-gray-700"
+                id="email"
+                placeholder="john@example.com"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
-              <div className="flex w-[80%] flex-col space-y-2">
-                <div className="flex flex-col space-y-2">
-                  <Label
-                    className="w-full text-left text-lg text-white"
-                    htmlFor="firstname"
-                  >
-                    First name
-                  </Label>
-                  <Input
-                    className="bg-[#e5d1b4] px-4 py-3 font-garamond text-lg font-semibold text-black placeholder:text-gray-700"
-                    id="firstname"
-                    placeholder="Johnny"
-                    type="text"
-                    value={firstname}
-                    onChange={(e) => setFirstname(e.target.value)}
-                  />
-                </div>
+            </div>
 
-                <div className="flex flex-col space-y-1">
-                  <Label
-                    className="w-full text-left text-lg text-white"
-                    htmlFor="username"
-                  >
-                    Username
-                  </Label>
-                  {usernameError && (
-                    <p className="mb-1 text-xs text-red-500">{usernameError}</p>
-                  )}
-                  <Input
-                    className="bg-[#e5d1b4] px-4 py-3 font-garamond text-lg font-semibold text-black placeholder:text-gray-700"
-                    id="username"
-                    placeholder="Johnny_"
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-                </div>
-              </div>
-
-              <div className="flex w-[80%] flex-col space-y-2">
-                <Label
-                  className="w-full text-left text-lg text-white"
-                  htmlFor="email"
-                >
-                  Email
-                </Label>
-                <Input
-                  className="w-full bg-[#e5d1b4] px-4 py-3 font-garamond text-lg font-semibold text-black placeholder:text-gray-700"
-                  id="email"
-                  placeholder="john@example.com"
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-
-              {/* Password */}
-              <div className="flex w-[80%] flex-col space-y-2">
-                <Label
-                  className="w-full text-left text-lg text-white"
-                  htmlFor="password"
-                >
-                  Password
-                </Label>
-                <Input
-                  className="w-full bg-[#e5d1b4] px-4 py-3 font-garamond text-lg font-semibold text-black placeholder:text-gray-700"
-                  id="password"
-                  placeholder="••••••••"
-                  type="password"
-                  value={password}
-                  onChange={(e) => {
-                    const val = e.target.value;
-
-                    setPassword(val);
-                    setPasswordError('');
-                    validatePassword(val);
-                  }}
-                />
-                {passwordRequirements.minLength &&
-                  passwordRequirements.hasUppercase &&
-                  passwordRequirements.hasSpecialChar && (
-                    <p className="text-s w-full text-green-500">
-                      Password meets the requirements.
-                    </p>
-                  )}
-              </div>
-
-              {/* Confirm Password */}
-              <div className="flex w-[80%] flex-col space-y-2">
-                <Label
-                  className="w-full text-left text-lg text-white"
-                  htmlFor="confirm-password"
-                >
-                  Confirm Password
-                </Label>
-                <Input
-                  className="w-full bg-[#e5d1b4] px-4 py-3 font-garamond text-lg font-semibold text-black placeholder:text-gray-700"
-                  id="confirm-password"
-                  placeholder="••••••••"
-                  type="password"
-                  value={confirmPassword}
-                  onChange={(e) => {
-                    setConfirmPassword(e.target.value);
-                    setPasswordError('');
-                  }}
-                />
-              </div>
-
-              {passwordError && (
-                <p className="mb-4 text-xs text-red-500">{passwordError}</p>
-              )}
-
-              <Button
-                className="w-[80%] transform bg-[#D8A730] px-10 py-3 text-xl font-semibold text-black shadow transition-transform hover:scale-105"
-                type="submit"
+            {/* Password */}
+            <div className="flex w-[80%] flex-col space-y-2">
+              <Label
+                className="w-full text-left text-lg text-white"
+                htmlFor="password"
               >
-                Sign up
-              </Button>
+                Password
+              </Label>
+              <Input
+                className="w-full bg-[#e5d1b4] px-4 py-3 font-garamond text-lg font-semibold text-black placeholder:text-gray-700"
+                id="password"
+                placeholder="••••••••"
+                type="password"
+                value={password}
+                onChange={(e) => {
+                  const val = e.target.value;
 
-              <Button
-                className="w-[80%] bg-[#bc6c25] px-4 py-3 text-xl font-semibold text-black shadow transition-transform hover:scale-105"
-                type="button"
-                onClick={() => router.push('/pages/home')}
+                  setPassword(val);
+                  setPasswordError('');
+                  validatePassword(val);
+                }}
+              />
+              {passwordRequirements.minLength &&
+                passwordRequirements.hasUppercase &&
+                passwordRequirements.hasSpecialChar && (
+                  <p className="text-s w-full text-green-500">
+                    Password meets the requirements.
+                  </p>
+                )}
+            </div>
+
+            {/* Confirm Password */}
+            <div className="flex w-[80%] flex-col space-y-2">
+              <Label
+                className="w-full text-left text-lg text-white"
+                htmlFor="confirm-password"
               >
-                Continue as Guest
-              </Button>
+                Confirm Password
+              </Label>
+              <Input
+                className="w-full bg-[#e5d1b4] px-4 py-3 font-garamond text-lg font-semibold text-black placeholder:text-gray-700"
+                id="confirm-password"
+                placeholder="••••••••"
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => {
+                  setConfirmPassword(e.target.value);
+                  setPasswordError('');
+                }}
+              />
+            </div>
 
-              <p className="mt-4 text-sm text-white dark:text-neutral-300">
-                Already have an account?{' '}
-                <Link
-                  className="text-[#D8A730] hover:underline"
-                  href="/auth/login"
-                >
-                  Log in.
-                </Link>
-              </p>
-            </form>
-          </div>
+            {passwordError && (
+              <p className="mb-4 text-xs text-red-500">{passwordError}</p>
+            )}
+
+            <Button
+              className="w-[80%] transform bg-[#D8A730] px-10 py-3 text-xl font-semibold text-black shadow transition-transform hover:scale-105"
+              type="submit"
+            >
+              Sign up
+            </Button>
+
+            <Button
+              className="w-[80%] bg-[#bc6c25] px-4 py-3 text-xl font-semibold text-black shadow transition-transform hover:scale-105"
+              type="button"
+              onClick={() => router.push('/pages/home')}
+            >
+              Continue as Guest
+            </Button>
+
+            <p className="mt-4 text-sm text-white dark:text-neutral-300">
+              Already have an account?{' '}
+              <Link
+                className="text-[#D8A730] hover:underline"
+                href="/auth/login"
+              >
+                Log in.
+              </Link>
+            </p>
+          </form>
         </div>
       </div>
     </>
